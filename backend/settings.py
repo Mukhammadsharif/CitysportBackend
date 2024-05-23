62a4dd4d7d0cd3b5b561187399ec9bb1572f8972
Mukhammadsharif <80579706+Mukhammadsharif@users.noreply.github.com>
2024-05-23 11:22:09 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -55,27 +55,25 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.citysportadmin\.uz$",
 ]
 
-CORS_ALLOW_METHODS = (
+CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
     "OPTIONS",
     "PATCH",
     "POST",
     "PUT",
-)
+]
 
-CORS_ALLOW_HEADERS = (
+CORS_ALLOW_HEADERS = [
     "accept",
     "authorization",
     "content-type",
     "user-agent",
     "x-csrftoken",
     "x-requested-with",
-)
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOW_HEADERS = default_headers + ('cache-control',)
 # Application definition
 
 INSTALLED_APPS = [
